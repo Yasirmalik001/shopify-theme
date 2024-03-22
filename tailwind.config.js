1d05ec4dce3cab43ac81e9ff8007fa23494b1385
YASIR MALIK <yasirrock6@gmail.com>
2024-03-22 16:42:37 +0500

    ci/cd pipeline

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./assets/*.liquid", "./layout/*.liquid", "./section/*.liquid", "./snippets/*.liquid", "./templates/*.liquid", "./templates/customers/*.liquid"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
